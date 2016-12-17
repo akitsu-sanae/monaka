bfeaa187987bdeca652b37341d4715cc1efddd80
akitsu <akitsu@ANSOPTERA>
2016-12-17 21:10:41 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monaka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
